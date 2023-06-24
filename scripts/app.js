acaa64aef115b88eaadf59144e02486a82bd54b3
Brett Kelley <brettkelley@geotab.com>
2023-06-24 18:56:41 +0200

    latest updates

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,8 @@
       app.pageItems.email = contactForm.querySelector('#email');
       app.pageItems.message = contactForm.querySelector('#message');
       app.pageItems.contactForm.onsubmit = contactFormSubmit();
+      // app.pageItems.skillsDiv = document.getElementById('skills');
+      app.pageItems.skillsUl = document.querySelector('#skills ul');
    };
 
    function contactFormSubmit(e) {
@@ -19,6 +21,21 @@
       app.pageItems.name.value = '';
       app.pageItems.email.value = '';
       app.pageItems.message.value = '';
+   }
+
+   app.addSkills = async function() {
+      const skills = await fetch('skills.json');
+      //const rawData = await fetch('sitedata.json');
+      const data = await skills.json();
+
+      for (let i = 0; i < data.skills.length; i++) {
+         console.log(data.skills[i]);
+         for (let j = 0; j < data.skills[i].length; j++) {
+            console.log('inner for loop');
+            console.log(data.skills[i][j]);
+            // const element = data.skills[i][j].length     
+         }
+      }
    }
 
 })(window.app = window.app || {});
